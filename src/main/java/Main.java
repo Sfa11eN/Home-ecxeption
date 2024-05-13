403ab510be74a74e2e07e85eb9c55c94f7e3b80f
Sfa11eN <sfallenn@gmail.com>
2024-05-13 13:00:54 +0300

    Final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,26 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Исключения");
+        check("login","pass", "pass");
+        check("loginloginloginloginlogin","pass", "pass");
+        check("login","passpasspasspasspasspass", "pass");
+        check("login","pass", "pass2");
+        check("login%%%%%","pass", "pass");
+        check("login","pass%%%%%", "pass");
+
 
     }
 
     private static void check(String login, String pass, String confirmPass) {
+        try {
+            checkLogin(login);
+            checkPass(pass, confirmPass);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка с введенным логином: " + e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка с введенным паролем" + e.getMessage());
+        }
+
     }
 
     private static void checkLogin(String login) throws WrongLoginException {
@@ -14,7 +30,20 @@ public class Main {
             throw new WrongLoginException("Логин должен содержать в себе только латинские буквы, цифры и знак подчеркивания.");
         }
         if (login.length() > 20) {
-            throw new WrongLoginException("Логин не может быть длиннее 20 букв");
+            throw new WrongLoginException("Логин не может быть длиннее 20 символов");
+        }
+
+    }
+
+    private static void checkPass(String pass, String confirmPass) throws WrongLoginException, WrongPasswordException {
+        if (!pass.matches(Validation)) {
+            throw new WrongPasswordException("Пароль должен содержать в себе только латинские буквы, цифры и знак подчеркивания.");
+        }
+        if (pass.length() > 20) {
+            throw new WrongPasswordException("Пароль не может быть длиннее 20 символов");
+        }
+        if (!pass.equals(confirmPass)) {
+            throw new WrongPasswordException("Пароли не совпадают");
         }
 
     }
